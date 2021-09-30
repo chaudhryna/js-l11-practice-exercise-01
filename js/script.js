f2f21bbc4fbb278f886cb47ec34958b3c437e37d
Nancy Chaudhry <nancych14@gmail.com>
2021-09-30 19:05:19 +0500

    Change the number of users displayed.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector(".num-users");
 
-const getData = async function () {
+const getData = async function (numUsers) {
   const usersRequest = await fetch(
-    "https://randomuser.me/api?results=5"
+    `https://randomuser.me/api?results=${numUsers}`
   );
   const data = await usersRequest.json();
   const userResults = data.results;
   displayUsers(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function(userResults) {
     randomFolks.innerHTML = "";
@@ -27,3 +28,7 @@ const displayUsers = function(userResults) {
     }
 }
 
+selectUserNumber.addEventListener('change', function(e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+})
