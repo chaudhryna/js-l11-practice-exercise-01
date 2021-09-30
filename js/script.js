e2bca3ad7dfd5cdb663253a9769f7a2e4e7c2782
Nancy Chaudhry <nancych14@gmail.com>
2021-09-30 18:49:17 +0500

    First commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+  const usersRequest = await fetch(
+    "https://randomuser.me/api?results=5"
+  );
+  const data = await usersRequest.json();
+  const userResults = data.results;
+  displayUsers(userResults);
+};
+
+getData();
+
+const displayUsers = function(userResults) {
+    randomFolks.innerHTML = "";
+    for (let user of userResults) {
+        let name = user.name.first;
+        let imageUrl = user.picture.medium;
+        let country = user.location.country;
+        let userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+    `;
+        randomFolks.append(userDiv);
+    }
+}
+
